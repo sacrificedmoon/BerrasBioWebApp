8dd3fc1e02da34f4c2f875231c06f7122cd926ad
AndersOrlinder <anders_orlinder@hotmail.com>
2020-04-07 02:06:39 +0200

    Dependencies & Scaffold Database
    Added dependencies:
    RuneTimeCompilation
    EF-core, EF-SQL, EF-tools
    Scaffold BerrasBioDb into models folder

---
+++ BerrasBioWebApp/BerrasBioWebApp/Models/FilmSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BerrasBioWebApp.Models
+{
+    public partial class FilmSchedule
+    {
+        public int Id { get; set; }
+        public int Filmid { get; set; }
+        public int Salonid { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Showtime { get; set; }
+        public int Freechairs { get; set; }
+        public bool Fullybooked { get; set; }
+
+        public virtual Films Film { get; set; }
+        public virtual Salons Salon { get; set; }
+    }
+}
---
+++ BerrasBioWebApp/BerrasBioWebApp/Models/Films.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BerrasBioWebApp.Models
+{
+    public partial class Films
+    {
+        public Films()
+        {
+            FilmSchedule = new HashSet<FilmSchedule>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public TimeSpan Length { get; set; }
+
+        public virtual ICollection<FilmSchedule> FilmSchedule { get; set; }
+    }
+}
